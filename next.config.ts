070be0368b5d367304d107d83a446eb7ba91ce27
Min Kyaw Thu <108884741+KyawThu12345@users.noreply.github.com>
2026-06-07 20:57:18 +0630

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: 'export', 
+  basePath: '/KyawThu12345.github.io',
+  assetPrefix: '/KyawThu12345.github.io',
+  images: {
+    unoptimized: true, 
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
